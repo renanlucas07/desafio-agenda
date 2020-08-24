e09abe1e1046af8c3f4179ddc617491b740004f4
Renan Lucas <renan.lucas07@gmail.com>
2020-08-24 08:17:49 -0300

    add tests for messages and users controllers

--- spec/controllers/api/v1/messages_controller_spec.rb
+++ spec/controllers/api/v1/messages_controller_spec.rb
@@ -14,19 +14,107 @@ RSpec.describe Api::V1::MessagesController, type: :controller do
   describe '#index' do
     before do
       allow_any_instance_of(Api::V1::MessagesController).to receive(:current_api_user) { master }
-      @headers = { 'Authorization' => master.token }
     end
 
     context 'when user has master permission' do
       it 'lists all messages that are not archived' do
 
-        request.headers.merge! @headers
+        request.headers.merge!({ 'Authorization' => master.token })
         get :index
 
         expect(response).to have_http_status(:ok)
         expect(response.content_type).to eq("application/json")
       end
     end
+
+    context 'when user has normal permission' do
+      it 'lists all messages that were sent to the user and are not archived' do
+        request.headers.merge!({ 'Authorization' => user.token })
+        get :index
+
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq("application/json")
+      end
+    end
+
+    context 'when user token is invalid' do
+      it 'receives an error message' do
+        request.headers.merge!({ 'Authorization' => user.token })
+        get :index
+
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.content_type).to eq("application/json")
+      end
+    end
   end
 
+  describe '#create' do
+    before do
+      allow_any_instance_of(Api::V1::MessagesController).to receive(:current_api_user) { user }
+    end
+
+    context 'when user creates a message' do
+      it 'receives the message created' do
+
+        request.headers.merge!({ 'Authorization' => user.token })
+        post :create, params: { title: 'message 1', content: 'message content',
+                                receiver_email: user1.email }
+
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq("application/json")
+      end
+    end
+
+    context 'when message is invalid' do
+      it 'returns validation errors' do
+        request.headers.merge!({ 'Authorization' => user.token })
+        post :create, params: { title: 'message 1', receiver_email: user1.email }
+
+        expect(response).to have_http_status(:bad_request)
+        expect(response.content_type).to eq("application/json")
+        expect(assigns(:message)).to_not be_valid
+
+      end
+    end
+  end
+
+  describe '#sent' do
+    before do
+      allow_any_instance_of(Api::V1::MessagesController).to receive(:current_api_user) { user }
+    end
+
+    it 'shows all messages sent by the user' do
+
+      request.headers.merge!({ 'Authorization' => user.token })
+      get :sent
+
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq("application/json")
+    end
+
+    context 'when user token is invalid' do
+      it 'receives an error message' do
+        request.headers.merge!({ 'Authorization' => user.token })
+        get :index
+
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.content_type).to eq("application/json")
+      end
+    end
+  end
+
+  describe '#show' do
+    before do
+      allow_any_instance_of(Api::V1::MessagesController).to receive(:current_api_user) { user }
+    end
+
+    it 'shows a message requested by the user if is unread, mark as read' do
+
+      request.headers.merge!({ 'Authorization' => user.token })
+      get :show, params: { id: message.id }
+
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq("application/json")
+    end
+  end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
+  let(:user) { FactoryBot.create(:user)}
 
+
+ describe '#show' do
+   before do
+     allow_any_instance_of(Api::V1::UsersController).to receive(:current_api_user) { user }
+   end
+
+   context 'when user has valid token' do
+     it 'show user profile' do
+       request.headers.merge!({ 'Authorization' => user.token })
+
+       get :index
+       expect(response).to have_http_status(:ok)
+     end
+   end
+ end
+
+ describe '#update' do
+   before do
+     allow_any_instance_of(Api::V1::UsersController).to receive(:current_api_user) { user }
+   end
+
+   context 'when user updates its profile' do
+     it 'updates user and returns the user that was updated' do
+       request.headers.merge!({ 'Authorization' => user.token })
+
+       patch :update, params: { user: {name: 'Editado', email: 'editado@email.com'}}
+       expect(response).to have_http_status(:ok)
+     end
+   end
+ end
 end
